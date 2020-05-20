3fb088c305eaa75e4efca72a155cbf63a1e0a31e
dinukaudan <dinuka.udan@gmail.com>
2020-05-20 10:22:15 +0530

    first commit

---
+++ src/main/java/com/udan/demo/MaySpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.udan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaySpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaySpringRestApplication.class, args);
+	}
+
+}
